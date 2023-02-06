19a1fd4d8383d8cea21c295330dba6e91b32abc5
Torumi <65400166+Torumi@users.noreply.github.com>
2023-02-06 15:29:43 +0200

    Ilja Caplinskis

--- main.py
+++ main.py
@@ -1,39 +1,40 @@
 organizations = [
     {
-        'name':'Tesla Motors',
-        'adress':'Tesla ave 1, USA',
-        'id':'8646678',
-        'contacts':[
+        'name': 'Tesla Motors',
+        'adress': 'Tesla ave 1, USA',
+        'id': '8646678',
+        'contacts': [
             {
-                'name':'Elon',
-                'position':'CEO',
-                'id':1
+                'name': 'Elon',
+                'position': 'CEO',
+                'id': 1
             },
             {
-                'name':'Jenifer',
-                'position':'CTO',
-                'id':2
+                'name': 'Jenifer',
+                'position': 'CTO',
+                'id': 2
             }
         ]
     },
     {
-            'name':'Apple',
-            'adress':'Apple Inc., 1 Infinite Loop, Cupertino, CA 95014, USA.',
-            'id':'942404110',
-            'contacts':[
-                {
-                    'name':'Tim',
-                    'position':'CEO',
-                    'id':1
-                },
-                {
-                    'name':'Steve',
-                    'position':'CTO',
-                    'id':2
-                }
-            ]
-        }
+        'name': 'Apple',
+        'adress': 'Apple Inc., 1 Infinite Loop, Cupertino, CA 95014, USA.',
+        'id': '942404110',
+        'contacts': [
+            {
+                'name': 'Tim',
+                'position': 'CEO',
+                'id': 1
+            },
+            {
+                'name': 'Steve',
+                'position': 'CTO',
+                'id': 2
+            }
+        ]
+    }
 ]
+
 
 def print_info(organization):
     print(f'''
@@ -41,7 +42,26 @@ Organization:
 {organization.get('name')} ({organization.get('id')})
 Adress: {organization.get('adress')},
 Contacts: {len(organization.get('contacts'))}
-{'_'*20}
+{'_' * 20}
             ''')
 
-print_info(organizations[0])
+
+def add_contact(organisation, name: str, position: str, id: int):
+    organisation.get('contacts').append({
+        'name': name,
+        'position': position,
+        'id': id
+    })
+
+
+
+while True:
+    print_info(organizations[0])
+    response = input('Add contact? (Y/N)')
+    if response == 'Y':
+        name = input('Name: ')
+        position = input('Position: ')
+        id = int(input('ID: '))
+        add_contact(organizations[0], name, position, id)
+    else:
+        break
