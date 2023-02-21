effad3e8471f9b96a559ce5908902bcd6ce6d239
Torumi <iljacap@gmail.com>
2023-02-22 01:27:01 +0200

    Update main.py - FIXME Rewrite get_organization_by_id function (improve speed) - Fixed bugs:     -- critical error in select_organization func     -- critical errors and bugs in delete_organization and delete_contact funcs

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import sys
 import json
 
-'''Load organizations'''
+'''Loads organizations from file'''
 with (open('organizations.json', 'r')) as openfile:
     organizations = json.load(openfile)
 
@@ -11,12 +11,13 @@ def select_organization():
     if len(organizations) != 0:
         for i in range(len(organizations)):
             print(f"{i} - {organizations[i].get('name')}")
+
         while True:
-            selected_organization_num = int(input("Enter number of organization: "))
             try:
+                selected_organization_num = int(input("Enter number of organization: "))
                 global selected_organization
                 selected_organization = organizations[selected_organization_num]
-            except IndexError:
+            except (IndexError, ValueError):
                 print("Invalid number")
             else:
                 break
@@ -32,6 +33,7 @@ Organization:
     Adress: {selected_organization.get('adress')},
     Income: {selected_organization.get('income')}$
     Contacts:''')
+
     for contact in selected_organization.get('contacts'):
         print(f"        {contact.get('name')} ({contact.get('position')}) ID: {contact.get('id')}")
     print('_' * 50)
@@ -42,6 +44,7 @@ def add_contact():
     name = input('Name: ')
     position = input('Position: ')
     contact_id = int(input('ID: '))
+
     selected_organization.get('contacts').append({
         'name': name,
         'position': position,
@@ -52,22 +55,23 @@ def add_contact():
 def delete_contact():
     """Deletes a contact of selected organization"""
     contacts = selected_organization.get('contacts')
+
     if len(contacts) != 0:
         for i in range(len(contacts)):
             print(f"{i} - {contacts[i].get('name')} ({contacts[i].get('position')}) ID: {contacts[i].get('id')}")
+
         while True:
-            contact_to_del_num = int(input("Enter number of contact to delete: "))
-            confirm = input('\nAre you sure?[Y/N upper case]')
-            if confirm == 'Y':
-                try:
-                    deleted_contact = selected_organization.get('contacts').pop(contact_to_del_num)
-                except IndexError:
-                    print("Invalid number")
-                else:
-                    print(
-                        f"{deleted_contact.get('name')} ({deleted_contact.get('position')}) ID: {deleted_contact.get('id')} HAS BEEN DELETED")
-                    break
+            try:
+                contact_to_del_num = int(input("Enter number of contact to delete: "))
+                contacts[contact_to_del_num]
+            except (IndexError, ValueError):
+                print("Invalid number")
             else:
+                confirm = input('\nAre you sure?[Y/N upper case]')
+                if confirm == 'Y':
+                    deleted_contact = selected_organization.get('contacts').pop(contact_to_del_num)
+                    print(
+                            f"{deleted_contact.get('name')} ({deleted_contact.get('position')}) ID: {deleted_contact.get('id')} HAS BEEN DELETED")
                 break
     else:
         print('There are no contacts yet')
@@ -100,36 +104,45 @@ def delete_organization():
         for i in range(len(organizations)):
             print(f"{i} - {organizations[i].get('name')}")
         while True:
-            organization_to_del_num = int(input("Enter number of organization to delete: "))
-            confirm = input('Are you sure?[Y/N upper case] ')
-            if confirm == 'Y':
-                try:
-                    deleted_organization = organizations.pop(organization_to_del_num)
-                except IndexError:
-                    print("Invalid number")
-                else:
-                    print(f"{deleted_organization.get('name')} HAS BEEN DELETED")
-                    break
+            try:
+                organization_to_del_num = int(input("Enter number of organization to delete: "))
+                organizations[organization_to_del_num]
+            except (IndexError, ValueError):
+                print("Invalid number")
             else:
+                confirm = input('\nAre you sure?[Y/N upper case]')
+                if confirm == 'Y':
+                    deleted_organization = organizations.pop(organization_to_del_num)
+                    print(f"{deleted_organization.get('name')} HAS BEEN DELETED")
                 break
     else:
         print('There are no organizations yet')
+
 
 def add_income():
     income = int(input("Income: "))
     selected_organization["income"] = income
 
+
 def print_income_top():
     sorted_orgs = list(reversed(sorted(organizations, key=lambda d: d['income'])))
     for index, company in enumerate(sorted_orgs[:5]):
-        print(f"{index+1} - {company.get('name')} ({company.get('income')}$)")
+        print(f"{index + 1} - {company.get('name')} ({company.get('income')}$)")
+
 
 def get_organization_by_id():
-    ID = input("ID: ")
-    for organization in organizations:
-        if organization.get('id') == ID:
+    id_list = [org.get('id') for org in organizations]
+    while True:
+        org_id = input("ID: ")
+        try:
+            index = id_list.index(org_id)
+        except ValueError:
+            print('No such ID')
+        else:
             global selected_organization
-            selected_organization = organization
+            selected_organization = organizations[index]
+            break
+
 
 def main():
     while True:
